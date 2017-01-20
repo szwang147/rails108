496420d53c550584d95ac41e8b858e6ed7d9bfd5
simon <wangzd.fudan@gmail.com>
2017-01-21 03:43:27 +0800

    implement post edit delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,12 +21,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to account_posts_path
+    flash[:notice] = "update post"
+  else
+    render :edit
+  end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to account_posts_path
+      flash[:notice] = "delete the post"
   end
 
 
