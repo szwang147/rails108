5b9cd3e043a9514186f5d5ed866deabfba600355
simon <wangzd.fudan@gmail.com>
2017-01-21 02:35:02 +0800

    implement join and quit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,7 @@ before_action :check_group_and_permission, only: [:edit, :update, :destroy]
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
       flash[:notice] = "create"
     else
@@ -44,6 +45,30 @@ before_action :check_group_and_permission, only: [:edit, :update, :destroy]
     redirect_to groups_path
     flash[:alert] = "delete"
   end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "welcome add this group"
+    else
+      flash[:alert] = "you already been the team member"
+    end
+    redirect_to group_path(@group)
+  end
+
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "quit this group"
+    else
+      flash[:alert] = "you are not team member how to quit"
+    end
+    redirect_to group_path(@group)
+  end
+
 
 
   private
